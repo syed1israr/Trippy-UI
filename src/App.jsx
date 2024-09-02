10b93c3a69eb732f80f0487d8532d39e9cc27744
Syed israr <121382749+syed1israr@users.noreply.github.com>
2024-09-02 10:42:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './Components/Header.jsx';
 const App = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+  <Header/>
       <main className="w-full max-w-4xl p-4">
         <div className="bg-white p-6 rounded-lg shadow-lg">
           <h1 className="text-2xl font-bold mb-4">Plan My Trip</h1>
